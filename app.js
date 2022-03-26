f22341ccb963aecde05fb5fa96268455dda0bb55
xandermeyen <xander.meyen@gmail.com>
2022-03-26 17:40:58 +0100

    commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const game = () => {
+    let pScore = 0;
+    let cScore = 0;
+
+    const startGame = () => {
+        const playBtn = document.querySelector('.intro button');
+        const introScreen = document.querySelector('.intro');
+        const match = document.querySelector('.match');
+
+        playBtn.addEventListener('click', () => {
+            introScreen.classList.add('fadeOut');
+            match.classList.add('fadeIn');
+        });
+    };
+
+    const playMatch = () => {
+        const options = document.querySelectorAll('.options button')
+    }
+
+startGame();
+
+};
+
+game();
