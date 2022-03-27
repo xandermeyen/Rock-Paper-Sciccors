4ac28b00aeaccc226beabbd09ce2ec5cd18b0ee6
xandermeyen <xander.meyen@gmail.com>
2022-03-27 12:26:41 +0200

    master

--- app.js
+++ app.js
@@ -17,23 +17,40 @@ const game = () => {
         const options = document.querySelectorAll('.options button');
         const playerHand = document.querySelectorAll('.player-hand');
         const computerHand = document.querySelectorAll('.computer-hand');
+        const hands = document.querySelectorAll('.hands img');
+
+hands.forEach(hand =>{
+    hand.addEventListener('animationend', function(){
+        this.style.animation = "";
+    });
+})
 
         const computerOptions = ['Rock', 'Paper', 'Sciccors'];
 
         options.forEach(options=>{
                 options.addEventListener('click', function(){
                     const computerNumber = Math.floor(Math.random() * 3);
-                    const computerChoise =  computerOptions[computerNumber];
+                    const computerChoice =  computerOptions[computerNumber];
                     
                     compareHands(this.textContent, computerChoise);
 
                    playerHand.src = `./assets/${this.textContent}.png`; 
-                   compareHands.src = `./assets/${computerChoise}.png`;
+                   computerHand.src = `./assets/${computerChoice}.png`;
+
+                   playerHand.style.animation = "shakePlayer 2s ease";
+                   computerHand.style.animation = "shakeComputer 2s ease";
             });
         });
     };
 
-const compareHands = (playerChoice, computerChoice) =>{
+    const updateScore = () => {
+        const playerScore = document.querySelector('.player-score p');
+        const computerScore = document.querySelector('.computer-score p');
+        playerScore.textContent = pScore;
+        computerScore.textContent = cScore;
+    }
+
+const computerHand = (playerChoice, computerChoice) =>{
     const winner = document.querySelector('.winner');
     if(playerChoice === computerChoice){
         winner.textContent = 'It is a tie :(';
@@ -44,9 +61,13 @@ const compareHands = (playerChoice, computerChoice) =>{
     if(playerChoice === 'Rock'){
         if(computerChoice === 'Sciccors' ){
             winner.textContent = 'Player wins';
+            pScore++;
+            updateScore();
             return;
         }else{
             winner.textContent = 'AI wins';
+            cScore++;
+            updateScore();
             return;
         }
     }
@@ -54,9 +75,13 @@ const compareHands = (playerChoice, computerChoice) =>{
     if(playerChoice === 'Paper'){
         if(computerChoice === 'Sciccors' ){
             winner.textContent = 'AI wins';
+            cScore++;
+            updateScore();
             return;
         }else{
             winner.textContent = 'Player wins';
+            pScore++;
+            updateScore();
             return;
         }
     }
@@ -64,9 +89,13 @@ const compareHands = (playerChoice, computerChoice) =>{
     if(playerChoice === 'Sciccors'){
         if(computerChoice === 'Rock' ){
             winner.textContent = 'AI wins';
+            cScore++;
+            updateScore();
             return;
         }else{
             winner.textContent = 'Player wins';
+            pScore++;
+            updateScore();
             return;
         }
     }
