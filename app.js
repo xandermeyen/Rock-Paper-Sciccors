9c56aab66a5045d2cacd429162320935627892a9
xandermeyen <xander.meyen@gmail.com>
2022-03-27 11:50:50 +0200

    master

--- app.js
+++ app.js
@@ -20,9 +20,57 @@ const game = () => {
 
         const computerOptions = ['rock', 'paper', 'sciccors'];
 
-        const computerNumber = Math.random() * 3;
-        console.log(computerNumber);
+        options.forEach(options=>{
+                options.addEventListener('click', function(){
+                    const computerNumber = Math.floor(Math.random() * 3);
+                    const computerChoise =  computerOptions[computerNumber];
+                    
+                    compareHands(this.textContent, computerChoise);
+
+                   playerHand.src = `./assets/${this.textContent}.png`; 
+                   compareHands.src = `./assets/${computerChoise}.png`;
+            });
+        });
+    };
+
+const compareHands = (playerChoice, computerChoice) =>{
+    const winner = document.querySelector('.winner');
+    if(playerChoice === computerChoice){
+        winner.textContent = 'It is a tie :(';
+        return;
     }
+
+    
+    if(playerChoice === 'rock'){
+        if(computerChoice === 'sciccors' ){
+            winner.textContent = 'Player wins';
+            return;
+        }else{
+            winner.textContent = 'AI wins';
+            return;
+        }
+    }
+
+    if(playerChoice === 'paper'){
+        if(computerChoice === 'sciccors' ){
+            winner.textContent = 'AI wins';
+            return;
+        }else{
+            winner.textContent = 'Player wins';
+            return;
+        }
+    }
+
+    if(playerChoice === 'sciccors'){
+        if(computerChoice === 'rock' ){
+            winner.textContent = 'AI wins';
+            return;
+        }else{
+            winner.textContent = 'Player wins';
+            return;
+        }
+    }
+};
 
 startGame();
 playMatch();
